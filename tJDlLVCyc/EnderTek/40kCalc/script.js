011f8e413040e04991aa0c4f7247f77edf3eb723
EnderUnknown <samkjell@yahoo.com>
2021-01-28 13:23:28 -0600

    Add files via upload

---
+++ tJDlLVCyc/EnderTek/40kCalc/script.js
@@ -0,0 +1,49 @@
+function getAttacks(){
+    return document.getElementById("att").value;
+}
+function getHits(){
+    return document.getElementById("bs").value;
+}
+
+function getWounds(){
+    return document.getElementById("w").value;
+}
+
+function getSaves(){
+    return document.getElementById("s").value;
+}
+
+function setPercent(num){
+    var v = (num)*100;
+    var s = v.toFixed(3);
+    document.getElementById("percent_value").innerText =  s + "%";
+}
+
+function setThrough(num){
+    document.getElementById("through_value").innerText = num.toFixed(3);
+}
+
+function doCalc(){
+    var a = (7-getHits())/6;
+    var b = (7-getWounds())/6;
+    var c = (getSaves()-1)/6;
+    var p = a * b * c;
+    setPercent(p);
+    var t = getAttacks() * p;
+    setThrough(t);
+}
+
+function doClear(){
+    document.getElementById("percent_value").innerText = "";
+    document.getElementById("through_value").innerText="";
+    document.getElementById("att").value = "";
+    document.getElementById("bs").value = "";
+    document.getElementById("w").value = "";
+    document.getElementById("s").value = "";
+}
+
+var calculate = document.getElementById("do_calc");
+calculate.addEventListener('click',function(){doCalc();});
+
+var clear = document.getElementById("do_clear");
+clear.addEventListener('click',function(){doClear();});
